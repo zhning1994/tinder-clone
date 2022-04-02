0a5f62c3720d9c3d4d44fdc6d1250f3d53fd58b5
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-04-02 18:30:38 +0800

    header component completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
-    <div className="">
-      <h1>This is Tinder clone!</h1>
+    <div className="app">
+      <Header />
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import PersonIcon from "@mui/icons-material/Person";
+import IconButton from "@mui/material/IconButton";
+import ChatIcon from '@mui/icons-material/Chat';
+import logo from './assets/tinder.svg';
+import './Header.css';
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <div className="header">
+      <IconButton>
+        <PersonIcon fontSize="large" className="header__icon" />
+      </IconButton>
+      <img 
+        className="header__logo"
+        src={logo}
+        alt="tinder-logo"
+      />
+      <IconButton>
+        <ChatIcon fontSize="large" className="header__icon" />
+      </IconButton>
+      
+    </div>
+  );
 }
 
-export default Header
+export default Header;
