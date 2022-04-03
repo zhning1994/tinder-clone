27f3110836dbbdd63881713368d1fd7d8697b652
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-04-03 19:12:55 +0800

    swipe button component completed

---
+++ src/SwipeButtons.js
@@ -0,0 +1,33 @@
+import "./SwipeButtons.css";
+import IconButton from "@mui/material/IconButton";
+import ReplayIcon from '@mui/icons-material/Replay';
+import CloseIcon from '@mui/icons-material/Close';
+import StarRateIcon from '@mui/icons-material/StarRate';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FlashOnIcon from '@mui/icons-material/FlashOn';
+
+
+
+function SwipeButtons() {
+    return (
+        <div className="swipeButtons">
+            <IconButton className="swipeButtons__repeat">
+                <ReplayIcon fontSize="large" />
+            </IconButton>
+            <IconButton className="swipeButtons__left">
+                <CloseIcon fontSize="large" />
+            </IconButton>
+            <IconButton className="swipeButtons__star">
+                <StarRateIcon fontSize="large" />
+            </IconButton>
+            <IconButton className="swipeButtons__right">
+                <FavoriteIcon fontSize="large" />
+            </IconButton>
+            <IconButton className="swipeButtons__lightning">
+                <FlashOnIcon fontSize="large" />
+            </IconButton>
+        </div>
+    )
+}
+
+export default SwipeButtons
